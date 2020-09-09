0f3df75efcd1519c3210e168ba88f9ae8db08972
ZebraPoacher <ZebraPoacher@users.noreply.github.com>
2020-09-09 17:11:09 +0200

    quotes and pempem

--- bot.py
+++ bot.py
@@ -51,13 +51,24 @@ async def awy(ctx):
     'the fking guy could make a movie about teletubbies and super mario and make it to be epic and best out there',
     'tho that system for banning is to retarded\nlike sure i tell ppl to go and buy artifical arms on ebay and brain cells\nsure i tell them that they are grass eating animals and dumb motherfuckers\nbut all the time toxic ppl are like "AA FUCK YOUR MOM,AAA WISH YOU CANCER FOR YOUR FAMILY AAAA KILL YOURSELF"\ni never insulted anyones family or smth i just tell ppl that they are dumb fucks\nand mostly those toxic ppl above get triggered and report me and i get banned they do not\nit\'s stuipid\ni mean telling somone that he is dumb is a fact not so much of an insult like wishing him and his family misfortune',
     'Awy is not savage\ncene we Serbs are maybe poor\nbut we aint no mongoloids',
+    'Fuck it why would i buy shadowlands when i can go on facebook and build up ma goddamn farm on farmvile\nor play that tower climb\nshit',
+    'why are league pl players becoming even stuipider\nyou guys are becoming more mentally disabled then us Serbs',
+    'hmmm this morring pigeon flied into my room\n',
+    'he had some avatar on dsicord of SS army or somethig,pretty good behaivour of an staff member gj warlame',
+    'that retard was saying in a room full of people that Hitler was verry smart man and that they should let him exterminate gypsies and Serbs and jews\nthen he said something about ma momma\nso i said that it\'s not his fault for being degenerate\nand that it\'s his mother\'s fault for releasing such a disgusting thing in this world',
+    'but yeh it is wat it is',
+    'Once i found reasonable polish player and he told me this "i no spik ing, i pl"',
+    'when you see retarded imbecile you can\'t really try to reason with him',
+    'I JUST SAID "fuck off degenerates"\nand bem riot gave me a big NONO for that',
+    'this one is just for you,this is how tru serbs looks like,carrying machetes and drinking vodka vodka\nhttps://image.prntscr.com/image/llvyhI-RTXK7aK-HMGVidA.png',
+    'https://image.prntscr.com/image/yOEamt6ZTIypMNXKWQIBHg.png'
     'https://media.discordapp.net/attachments/364712407601512450/719636503676059668/Screenshot_1469.png?width=931&height=515',
     'https://media.discordapp.net/attachments/364712407601512450/719636518758777003/Screenshot_1470.png']
     rolled_quote = random.choice(awyQuotes)
     if rolled_quote.startswith('http'):
-        await ctx.send('Awy once said: {}.'.format(random.choice(awyQuotes)))
+        await ctx.send('rolled_quote')
     else:
-        await ctx.send('Awy once said: "{}".'.format(random.choice(awyQuotes)))
+        await ctx.send('Awy once said: "{}".'.format(rolled_quote))
 
 @client.command()
 async def akcent(ctx):
@@ -66,5 +77,9 @@ async def akcent(ctx):
 @client.command()
 async def stonelia(ctx):
     await ctx.send('https://media.discordapp.net/attachments/364712407601512450/709413405131407380/Screenshot_794.png?width=686&height=515')
+
+@client.command()
+async def pempem(ctx):
+    await ctx.send('https://media.discordapp.net/attachments/364712407601512450/752975692966264902/Screenshot_797.png?width=684&height=515')
 
 client.run(os.environ['DISCORD_TOKEN']) #token
