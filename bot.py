2adc1dbe73abc5291c78c187084a0f6b98916a4a
ZebraPoacher <ZebraPoacher@users.noreply.github.com>
2020-09-09 15:43:10 +0200

    added more awy quotes

--- bot.py
+++ bot.py
@@ -25,7 +25,20 @@ async def awy(ctx):
     'satan himself will get borred of trying to tourment me',
     'but you be pullin some Merlin shit summoning excalibuyrs worry not',
     'CENE YOU ARE MARIA TERESA',
-    'well ye i cheated on her but like cmone she has allready girlfriend if i find another one to throw asidewhy shodln\'t i 2000 iq decision making']
+    'well ye i cheated on her but like cmone she has allready girlfriend if i find another one to throw asidewhy shodln\'t i 2000 iq decision making',
+    'she left me saying that i am d og shit',
+    'sh was bisexual and hed lesbian girlfriend',
+    'Is brnabic fokin tranvestit ?',
+    'like ma useless old men says "everyone washappy with pay checks in communism now they talk shit"',
+    'O PARTIGANOOOO,PORTAMI VIAAAAAA',
+    'i even got called by ma girl friend a "man whore "',
+    'we\'ll invite our boi Dran to gather up mats',
+    'looks like i have to educate you',
+    'it\'s better strategy than telling  them that they are single atomed  ameba only for them to go from 0/5 to 0/10',
+    'did you treshtalck me',
+    'you can\'t determine who\'s a selfish scumbag and who\'s selfless good guy if you only try to se good in them',
+    'well i always try to see the worst in people first,that way you get much more realistic vision of them and don\'t get hopes up for them to in the end dissapoint you',
+    'seggy is understanding how the world is functioning']
     await ctx.send('Awy once said: "{}".'.format(random.choice(awyQuotes)))
 
 
