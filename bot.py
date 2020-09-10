bdc2213ae825455a1f8db66bb66af749d61d4f13
ZebraPoacher <ZebraPoacher@users.noreply.github.com>
2020-09-10 14:24:49 +0200

    fix change for az

--- bot.py
+++ bot.py
@@ -204,8 +204,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    # if message.author.id == 329341017914605569:
-    if message.author.display_name == "diethylamide":
+    if message.author.id == 329341017914605569:
         year = message.created_at.year
         month = message.created_at.month
         day = message.created_at.day
