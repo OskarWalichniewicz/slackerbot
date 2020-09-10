f4111e46f8ce71bd113b60fbf701059f5407a6c6
ZebraPoacher <ZebraPoacher@users.noreply.github.com>
2020-09-10 12:56:12 +0200

    calculating difference

--- bot.py
+++ bot.py
@@ -268,7 +268,9 @@ async def az(ctx): # az id = 329341017914605569
         json_dict["hour"],
         json_dict["minute"],
         json_dict["second"])
-    print(az_date)
+    current_date = datetime.now()
+    diff = current_date - az_date
+    print(diff)
 
 
 client.run(os.environ['DISCORD_TOKEN']) #token
