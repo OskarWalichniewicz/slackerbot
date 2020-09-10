7a92554d22a1ff78c038321c0a854b261dc3f8ae
ZebraPoacher <ZebraPoacher@users.noreply.github.com>
2020-09-10 11:51:39 +0200

    aaa

--- bot.py
+++ bot.py
@@ -240,9 +240,9 @@ async def ignios(ctx):
 async def az(ctx): # az id = 329341017914605569
     user_id = 329341017914605569
     for channel in ctx.guild.text_channels:
-        fetchMessage = await channel.history().find(lambda m: m.author.id == user_id)
-        print(fetchMessage)
-        sent = str(fetchMessage.created_at)
+        async for message in channel.history(limit=None):
+            if message.author.id == user_id:
+                sent = str(message.created_at)
         await ctx.send('last msg at ' + sent)
         break
 #awys chamber = 364712407601512450
