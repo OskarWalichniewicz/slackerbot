58cd1c102d8bfb1c18d0af502888d849bc51458f
ZebraPoacher <ZebraPoacher@users.noreply.github.com>
2020-09-11 11:16:59 +0200

    added activity

--- bot.py
+++ bot.py
@@ -369,6 +369,7 @@ client = commands.Bot(command_prefix = '.')
 
 @client.event
 async def on_ready():
+    await client.change_presence(activity=discord.Game('Milica is still a midget.'))
     print("Bot ready")
 
 @client.event
