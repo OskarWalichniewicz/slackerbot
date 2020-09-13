496095d384714617f51aec0d809be24928d16fd7
ZebraPoacher <ZebraPoacher@users.noreply.github.com>
2020-09-13 19:03:10 +0200

    trying word of a day

---
+++ wordofaday.py
@@ -0,0 +1,23 @@
+from random_word import RandomWords
+from googletrans import Translator
+import cyrtranslit
+
+r = RandomWords()
+translator = Translator()
+
+def get_word_of_the_day():
+    english_word = r.word_of_the_day()
+
+    serbian_word_cyr = translator.translate(english_word, dest = 'sr').text
+    serbian_word_lat = cyrtranslit.to_latin(serbian_word_cyr)
+    serbian_word = serbian_word_cyr + " / " + serbian_word_lat
+
+    italian_word = translator.translate(english_word, dest = 'it').text
+
+    dutch_word = translator.translate(english_word, dest = 'nl').text
+
+    polish_word = translator.translate(english_word, dest = 'pl').text
+
+    romanian_word = translator.translate(english_word, dest = 'ro').text
+
+    return english_word, serbian_word, italian_word, dutch_word, polish_word, romanian_word
