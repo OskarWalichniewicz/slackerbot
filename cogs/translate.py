22b4d0bb9083a5f1f6d5711243ef394743f022e6
ZebraPoacher <ZebraPoacher@users.noreply.github.com>
2020-09-13 14:43:13 +0200

    fixes to names of commands

--- cogs/translate.py
+++ cogs/translate.py
@@ -13,28 +13,28 @@ class Translate(commands.Cog):
         print('[COG] Translate ready.')
 
     @commands.command()
-    async def translate_serbian(self, ctx, text):
+    async def serbian(self, ctx, text):
         outp_cyr = translator.translate(text, dest = 'sr').text
         outp_lat = cyrtranslit.to_latin(outp_cyr)
         await ctx.send(str(outp_cyr + "\n" + outp_lat))
 
     @commands.command()
-    async def translate_english(self, ctx, text):
+    async def english(self, ctx, text):
         outp = translator.translate(text, dest = 'en').text
         await ctx.send(str(outp))
 
     @commands.command()
-    async def translate_dutch(self, ctx, text):
+    async def dutch(self, ctx, text):
         outp = translator.translate(text, dest = 'nl').text
         await ctx.send(str(outp))
 
     @commands.command()
-    async def translate_italian(self, ctx, text):
+    async def italian(self, ctx, text):
         outp = translator.translate(text, dest = 'it').text
         await ctx.send(str(outp))
 
     @commands.command()
-    async def translate_polish(self, ctx, text):
+    async def polish(self, ctx, text):
         outp = translator.translate(text, dest = 'pl').text
         await ctx.send(str(outp))
 
