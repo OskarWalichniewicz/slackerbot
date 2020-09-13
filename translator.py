7c6eb7796a3dbd3d98a28462d4b5b6d92635c719
ZebraPoacher <ZebraPoacher@users.noreply.github.com>
2020-09-13 13:48:37 +0200

    added latin maybe

--- translator.py
+++ translator.py
@@ -3,7 +3,8 @@ from googletrans import Translator
 translator = Translator()
 
 def translate_serbian(text):
-    return str(translator.translate(text, dest = 'sr').text)
+    outp = translator.translate(text, dest = 'sr').text + " / " + translator.translate(text, dest = 'sr-Latn')
+    return str(outp)
 
 def translate_english(text):
     return str(translator.translate(text, dest = 'en').text)
