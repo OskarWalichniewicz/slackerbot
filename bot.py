da6cd832635460837cc2cc520d4783c1c63ea9bc
ZebraPoacher <ZebraPoacher@users.noreply.github.com>
2020-09-13 14:10:29 +0200

    added dutch and italian

--- bot.py
+++ bot.py
@@ -115,6 +115,14 @@ async def english(ctx, *, translate):
     await ctx.send(translate_english(translate))
 
 @client.command()
+async def dutch(ctx, *, translate):
+    await ctx.send(translate_dutch(translate))
+
+@client.command()
+async def italian(ctx, *, translate):
+    await ctx.send(translate_italian(translate))
+
+@client.command()
 async def pupinka(ctx):
     await ctx.send('https://cdn.discordapp.com/attachments/364712407601512450/399218303739887617/WoWScrnShot_010618_160818.jpg')
 
--- translator.py
+++ translator.py
@@ -9,3 +9,11 @@ def translate_serbian(text):
 def translate_english(text):
     outp = translator.translate(text, dest = 'en').text
     return str(outp)
+
+def translate_dutch(text):
+    outp = translator.translate(text, dest = 'nl').text
+    return str(outp)
+
+def translate_italian(text):
+    outp = translator.translate(text, dest = 'it').text
+    return str(outp)
