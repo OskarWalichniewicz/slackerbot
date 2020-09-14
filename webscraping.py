aae0ebe38380fa639efcb28988f21fdfa058031f
ZebraPoacher <ZebraPoacher@users.noreply.github.com>
2020-09-14 19:57:13 +0200

    i dont fucking know anymore

--- webscraping.py
+++ webscraping.py
@@ -2,17 +2,17 @@ from selenium import webdriver
 import time
 import os
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
-
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-
 def get_random_word():
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     driver.get('https://randomwordgenerator.com/')
 
     word = driver.find_element_by_id("result")
+    driver.delete_all_cookies()
     driver.quit()
     return str(word.text)
