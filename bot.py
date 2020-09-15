934a395b4575352da3cbe50e85b7016ad85220cf
ZebraPoacher <ZebraPoacher@users.noreply.github.com>
2020-09-15 12:23:46 +0200

    reverse activity changes

--- bot.py
+++ bot.py
@@ -3,13 +3,35 @@ from discord.ext import commands
 import os
 import asyncio
 from github_integration import *
-from activity import status_task
 
 client = commands.Bot(command_prefix = '.')
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
+
+async def status_task(wait_time):
+    while True:
+        await client.change_presence(activity=discord.Game('Milica is a midget'))
+        await asyncio.sleep(wait_time)
+        await client.change_presence(activity=discord.Game('Dran doesn\'t have mats'))
+        await asyncio.sleep(wait_time)
+        await client.change_presence(activity=discord.Game('Az is dead'))
+        await asyncio.sleep(wait_time)
+        await client.change_presence(activity=discord.Game('Cenelia is handsome'))
+        await asyncio.sleep(wait_time)
+        await client.change_presence(activity=discord.Game('Bobsy is being molested'))
+        await asyncio.sleep(wait_time)
+        await client.change_presence(activity=discord.Game('Awy is the wisest'))
+        await asyncio.sleep(wait_time)
+        await client.change_presence(activity=discord.Game('Akcent hates Cene'))
+        await asyncio.sleep(wait_time)
+        await client.change_presence(activity=discord.Game('Gazda likes Rasta'))
+        await asyncio.sleep(wait_time)
+        await client.change_presence(activity=discord.Game('Segment is old'))
+        await asyncio.sleep(wait_time)
+        await client.change_presence(activity=discord.Game('Tesla was croatian'))
+        await asyncio.sleep(wait_time)
 
 @client.event
 async def on_ready():
