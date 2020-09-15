49296219a1bc6685bee618836e0177d4bf4e9c4d
ZebraPoacher <ZebraPoacher@users.noreply.github.com>
2020-09-15 10:42:38 +0200

    added short aliases for translate

--- cogs/translate.py
+++ cogs/translate.py
@@ -12,33 +12,33 @@ class Translate(commands.Cog):
     async def on_ready(self):
         print('[COG] Translate ready.')
 
-    @commands.command()
+    @commands.command(aliases = ['serbian', 'rs', 'sr'])
     async def serbian(self, ctx, *, text):
         outp_cyr = translator.translate(text, dest = 'sr').text
         outp_lat = cyrtranslit.to_latin(outp_cyr)
         await ctx.send(str(outp_cyr + "\n" + outp_lat))
 
-    @commands.command()
+    @commands.command(aliases = ['english', '.en'])
     async def english(self, ctx, *, text):
         outp = translator.translate(text, dest = 'en').text
         await ctx.send(str(outp))
 
-    @commands.command()
+    @commands.command(aliases = ['dutch', '.nl'])
     async def dutch(self, ctx, *, text):
         outp = translator.translate(text, dest = 'nl').text
         await ctx.send(str(outp))
 
-    @commands.command()
+    @commands.command(aliases = ['italian', '.it'])
     async def italian(self, ctx, *, text):
         outp = translator.translate(text, dest = 'it').text
         await ctx.send(str(outp))
 
-    @commands.command()
+    @commands.command(aliases = ['polish', '.pl'])
     async def polish(self, ctx, *, text):
         outp = translator.translate(text, dest = 'pl').text
         await ctx.send(str(outp))
 
-    @commands.command()
+    @commands.command(aliases = ['romanian', '.ro'])
     async def romanian(self, ctx, *, text):
         outp = translator.translate(text, dest = 'ro').text
         await ctx.send(str(outp))
