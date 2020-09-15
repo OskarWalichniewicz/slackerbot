a9224e6708dbcd3411f3e6d4000c27e47bcab9ae
ZebraPoacher <ZebraPoacher@users.noreply.github.com>
2020-09-15 11:15:23 +0200

    changes to translate comm names

--- cogs/translate.py
+++ cogs/translate.py
@@ -12,34 +12,34 @@ class Translate(commands.Cog):
     async def on_ready(self):
         print('[COG] Translate ready.')
 
-    @commands.command(aliases = ['serbian', 'rs', 'sr'])
-    async def _serbian(self, ctx, *, text):
+    @commands.command(aliases = ['rs', 'sr'])
+    async def serbian(self, ctx, *, text):
         outp_cyr = translator.translate(text, dest = 'sr').text
         outp_lat = cyrtranslit.to_latin(outp_cyr)
         await ctx.send(str(outp_cyr + "\n" + outp_lat))
 
-    @commands.command(aliases = ['english', 'en'])
-    async def _english(self, ctx, *, text):
+    @commands.command(aliases = ['en'])
+    async def english(self, ctx, *, text):
         outp = translator.translate(text, dest = 'en').text
         await ctx.send(str(outp))
 
-    @commands.command(aliases = ['dutch', 'nl'])
-    async def _dutch(self, ctx, *, text):
+    @commands.command(aliases = ['nl'])
+    async def dutch(self, ctx, *, text):
         outp = translator.translate(text, dest = 'nl').text
         await ctx.send(str(outp))
 
-    @commands.command(aliases = ['italian', 'it'])
-    async def _italian(self, ctx, *, text):
+    @commands.command(aliases = ['it'])
+    async def italian(self, ctx, *, text):
         outp = translator.translate(text, dest = 'it').text
         await ctx.send(str(outp))
 
-    @commands.command(aliases = ['polish', 'pl'])
-    async def _polish(self, ctx, *, text):
+    @commands.command(aliases = ['pl'])
+    async def polish(self, ctx, *, text):
         outp = translator.translate(text, dest = 'pl').text
         await ctx.send(str(outp))
 
-    @commands.command(aliases = ['romanian', 'ro'])
-    async def _romanian(self, ctx, *, text):
+    @commands.command(aliases = ['ro'])
+    async def romanian(self, ctx, *, text):
         outp = translator.translate(text, dest = 'ro').text
         await ctx.send(str(outp))
 
