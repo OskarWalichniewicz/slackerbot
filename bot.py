ef251909d26003ef974d268b1a73751734118ef2
ZebraPoacher <ZebraPoacher@users.noreply.github.com>
2020-09-15 20:03:31 +0200

    typo

--- bot.py
+++ bot.py
@@ -68,7 +68,7 @@ on_ready is called when client (bot) is done preparing the data received from Di
 @client.event
 async def on_ready():
     client.loop.create_task(status_task(60)) # loops status_task in background
-    print("[BOT] Cleint ready.")
+    print("[BOT] Client ready.")
 
 """
 on_message is called when message is sent.
