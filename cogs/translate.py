f4c6299d1a1fa8faccae8dff8ab49a384ec23090
ZebraPoacher <ZebraPoacher@users.noreply.github.com>
2020-10-10 11:11:29 +0200

    added didyoumean

--- cogs/translate.py
+++ cogs/translate.py
@@ -1,7 +1,9 @@
 import discord
 from discord.ext import commands
 from googletrans import Translator
+from webscraping import *
 import cyrtranslit
+
 
 class Translate(commands.Cog):
 
@@ -12,38 +14,47 @@ class Translate(commands.Cog):
     async def on_ready(self):
         print('[COG] Translate ready.')
 
-    @commands.command(aliases = ['rs', 'sr'])
+    @commands.command(aliases=['rs', 'sr'])
     async def serbian(self, ctx, *, text):
-        outp_cyr = translator.translate(text, dest = 'sr').text
+        outp_cyr = translator.translate(text, dest='sr').text
         outp_lat = cyrtranslit.to_latin(outp_cyr)
         await ctx.send(str(outp_cyr + "\n" + outp_lat))
 
-    @commands.command(aliases = ['en'])
+    @commands.command(aliases=['en'])
     async def english(self, ctx, *, text):
-        outp = translator.translate(text, dest = 'en').text
+        outp = translator.translate(text, dest='en').text
         await ctx.send(str(outp))
 
-    @commands.command(aliases = ['nl'])
+    @commands.command(aliases=['nl'])
     async def dutch(self, ctx, *, text):
-        outp = translator.translate(text, dest = 'nl').text
+        outp = translator.translate(text, dest='nl').text
         await ctx.send(str(outp))
 
-    @commands.command(aliases = ['it'])
+    @commands.command(aliases=['it'])
     async def italian(self, ctx, *, text):
-        outp = translator.translate(text, dest = 'it').text
+        outp = translator.translate(text, dest='it').text
         await ctx.send(str(outp))
 
-    @commands.command(aliases = ['pl'])
+    @commands.command(aliases=['pl'])
     async def polish(self, ctx, *, text):
-        outp = translator.translate(text, dest = 'pl').text
+        outp = translator.translate(text, dest='pl').text
         await ctx.send(str(outp))
 
-    @commands.command(aliases = ['ro'])
+    @commands.command(aliases=['ro'])
     async def romanian(self, ctx, *, text):
-        outp = translator.translate(text, dest = 'ro').text
+        outp = translator.translate(text, dest='ro').text
         await ctx.send(str(outp))
+
+    @commands.command(aliases=['dym'])
+    async def didyoumean(self, ctx, *, query):
+        if query.contains(" "):
+            query.replace(" ", "+")
+        didyoumean = webscrap_didyoumean(origin_language=auto, query=query)
+        await ctx.send("Did you mean: {}".format(didyoumean))
+
 
 def setup(client):
     client.add_cog(Translate(client))
+
 
 translator = Translator()
--- webscraping.py
+++ webscraping.py
@@ -20,16 +20,20 @@ chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 
 # creates a webdriver with given path to chromedriver and previously set options.
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+driver = webdriver.Chrome(executable_path=os.environ.get(
+    "CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 """
 Opens randomwordgenerator.com site and gets the word from there.
 returns scrapped word (string)
 """
+
+
 def webscrap_word():
-    driver.get('https://randomwordgenerator.com/') # loads page
-    word = driver.find_element_by_id("result") # finds result
+    driver.get('https://randomwordgenerator.com/')  # loads page
+    word = driver.find_element_by_id("result")  # finds result
     return str(word.text)
+
 
 """
 Opens Google Search (images only) and gets all the images from the first page with given query.
@@ -38,37 +42,46 @@ params: query (str) - query (word, sentence) that we want to search in google im
         wait_time (int; default = 1) - number of seconds that driver waits before it starts scrapping (needed for site to loads)
 returns scrapped images (list)
 """
+
+
 def webscrap_google_images(query, number_of_imgs, wait_time=1):
     # creates a webdriver with given path to chromedriver and previously set options.
-    search_url = "https://www.google.com/search?tbm=isch&q={}".format(query) # tbm=isch means image
-    driver.get(search_url) # loads page
+    # tbm=isch means image
+    search_url = "https://www.google.com/search?tbm=isch&q={}".format(query)
+    driver.get(search_url)  # loads page
     image_urls = []
     image_count = 0
-    while image_count < number_of_imgs: # as long as list is not populated
-        thumbnail_results = driver.find_elements_by_css_selector("img.Q4LuWd") # get all image thumbnail results
+    while image_count < number_of_imgs:  # as long as list is not populated
+        thumbnail_results = driver.find_elements_by_css_selector(
+            "img.Q4LuWd")  # get all image thumbnail results
 
-        for img in thumbnail_results: # try to click every thumbnail, to show actual image
+        for img in thumbnail_results:  # try to click every thumbnail, to show actual image
             try:
                 img.click()
                 time.sleep(wait_time)
             except Exception:
                 continue
 
-            actual_images = driver.find_elements_by_css_selector('img.n3VNCb') # extract previously opened image's urls
+            actual_images = driver.find_elements_by_css_selector(
+                'img.n3VNCb')  # extract previously opened image's urls
             for actual_image in actual_images:
                 if actual_image.get_attribute('src') and 'http' in actual_image.get_attribute('src'):
                     image_urls.append(actual_image.get_attribute('src'))
             image_count = len(image_urls)
 
-            if len(image_urls) >= number_of_imgs: # if we get amount of images we asked for while calling function
+            # if we get amount of images we asked for while calling function
+            if len(image_urls) >= number_of_imgs:
                 break
 
     return image_urls
+
 
 """
 Opens mentalfloss.com/amazingfactgenerator and gets facts from there (text and image).
 returns image url (string), fact description (string)
 """
+
+
 def webscrap_fact():
     while True:
         url = 'https://www.mentalfloss.com/amazingfactgenerator'
@@ -79,42 +92,76 @@ def webscrap_fact():
         fact_descr = desc.get_attribute("data-description")
         return img_url, fact_descr
 
+
 """
 Using some-random-api (some-random-api.ml) and gets image from there.
 params: thing (string) - query, thing of which we want to get picture. List at (https://docs.some-random-api.ml/json/image)
 returns image url (string)
 """
+
+
 def webscrap_random_api(thing):
     url = 'https://some-random-api.ml/img/{}'.format(thing)
-    req = urlrequest.Request(url, headers={'User-Agent': 'Mozilla/5.0'}) # doesnt work without it (404 not found request)
+    # doesnt work without it (404 not found request)
+    req = urlrequest.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
     load_json = json.loads(urlrequest.urlopen(req).read())
     return load_json['link']
+
 
 """
 Using some-random-api (some-random-api.ml) and gets fact from there.
 params: thing (string) - query, thing of which we want to get fact. List at (https://docs.some-random-api.ml/json/facts)
 returns fact (string)
 """
+
+
 def webscrap_random_api_fact(thing):
     url = 'https://some-random-api.ml/facts/{}'.format(thing)
-    req = urlrequest.Request(url, headers={'User-Agent': 'Mozilla/5.0'}) # doesnt work without it (404 not found request)
+    # doesnt work without it (404 not found request)
+    req = urlrequest.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
     load_json = json.loads(urlrequest.urlopen(req).read())
     return load_json['fact']
+
 
 """
 Using random-d.uk API (https://random-d.uk/api) and gets duck picture from there.
 returns image url (string)
 """
+
+
 def webscrap_duck():
     url = 'https://random-d.uk/api/v2/random'
     req = urlrequest.Request(url)
     load_json = json.loads(urlrequest.urlopen(req).read())
     return load_json['url']
 
+
 """
 Using dog.ceo API (https://dog.ceo/dog-api/) and gets dog picture from there.
 returns image url (string)
 """
+
+
 def webscrap_dog():
-    load_json = json.loads(urlrequest.urlopen("https://dog.ceo/api/breeds/image/random").read().decode("utf-8"))
+    load_json = json.loads(urlrequest.urlopen(
+        "https://dog.ceo/api/breeds/image/random").read().decode("utf-8"))
     return load_json['message']
+
+
+"""
+params: origin language (auto or en)
+        destination language - can be anything
+        query - if spaces, replace them with +
+"""
+
+
+def webscrap_didyoumean(origin_language=auto, destination_language=en, query):
+    url = 'http://translate.google.com/#{}|{}|{}'.format(
+        origin_language, destination_language, query)
+    driver.get(url)
+    didyoumean = driver.find_element_by_id("spelling-correction")
+    didyoumean_text = didyoumean.text
+    separator = ':'
+    # seperate text with : (cuz its "Did you mean: [word]") and also removes first character (space)
+    didyoumean_final = didyoumean_text.split(separator, 1)[1][1:]
+    return didyoumean_final
