b0e19adfcbfbf511394a35dfe919a15c157d641a
ZebraPoacher <ZebraPoacher@users.noreply.github.com>
2020-10-10 11:18:45 +0200

    idk fix i hope

--- cogs/translate.py
+++ cogs/translate.py
@@ -49,7 +49,7 @@ class Translate(commands.Cog):
     async def didyoumean(self, ctx, *, query):
         if query.contains(" "):
             query.replace(" ", "+")
-        didyoumean = webscrap_didyoumean(origin_language='auto', query=query)
+        didyoumean = webscrap_didyoumean(query=query)
         await ctx.send("Did you mean: {}".format(didyoumean))
 
 
