12186117802c5677d99eeb6fddd2cf33070bcd76
ZebraPoacher <ZebraPoacher@users.noreply.github.com>
2020-10-12 17:49:59 +0200

    fix

--- mongoDB.py
+++ mongoDB.py
@@ -195,13 +195,13 @@ class MongoDB():
             )
 
         embed_leaderboard = discord.Embed(
-            title='Trivia leaderboard'
-            description=self.get_leaderboard_blueprint(0),
+            title='Trivia leaderboard',
+            description=get_leaderboard_blueprint(0),
             colour=discord.Color.green()
         )
         for x in range(1, len(sorted_leaderboard)):
             embed_leaderboard.add_field(
-                name='', value=self.get_leaderboard_blueprint(x))
+                name='', value=get_leaderboard_blueprint(x))
 
         return embed_leaderboard
 
