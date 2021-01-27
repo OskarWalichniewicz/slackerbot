69743105b16b9ea393514692641f2167396ff1f3
Oskar <nefil4563@gmail.com>
2021-01-27 17:18:50 +0100

    BlessRNG

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands, tasks
 import os
 import asyncio
-from datetime import time
+from datetime import datetime, time
 from reddit import *
 from Question import *
 from mongoDB import MongoDB
